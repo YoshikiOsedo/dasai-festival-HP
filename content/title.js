1e1c416aff0a61b5f4e16dbc6397519912444114
大脊戸 慶揮 <qinghuidajihu07311031@gmail.com>
2026-09-17 02:14:51 +0900

    Update title.js

--- content/title.js
+++ content/title.js
@@ -6,7 +6,7 @@ window.festivalContent.title = {
   "festivalName": "デー祭2026",
   "date": "10/31, 11/1, 11/2",
   "time": "",
-  "venue": "千葉大学　西千葉キャンパス　工学・情報系講義棟　1Fエキシビションギャラリー",
+  "venue": "千葉大学　西千葉キャンパス　工学・情報学系講義棟　1Fエキシビションギャラリー",
   "introduction": "※このページは現在編集中です。\n未確定情報を含みますので、情報が確定次第お知らせで告知します。",
   "logo": "./assets/logo.png",
   "logoAlt": "デーサイ企画部ロゴ — IDS CHIBA UNIV."
