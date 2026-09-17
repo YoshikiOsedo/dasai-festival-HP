e32f2f0b9cc3b0dac95d317e77ce0c77a559cc5d
大脊戸 慶揮 <qinghuidajihu07311031@gmail.com>
2026-09-17 12:19:59 +0900

    スライド写真の参照先を修正

--- content/slideshow.js
+++ content/slideshow.js
@@ -4,8 +4,9 @@ window.festivalContent.slideshow = {
   interval: 6000,
   transition: 1000,
   items: [
-    { src: "./assets/slideshow/demophoto1.jpg", alt: "スライド写真 1" },
-    { src: "./assets/slideshow/demophoto2.jpg", alt: "スライド写真 2" },
-    { src: "./assets/slideshow/demophoto3.jpg", alt: "スライド写真 3" }
+    { src: "./assets/slideshow/demo1.jpg", alt: "スライド写真 1" },
+    { src: "./assets/slideshow/demo2.jpg", alt: "スライド写真 2" },
+    { src: "./assets/slideshow/demo3.jpg", alt: "スライド写真 3" },
+    { src: "./assets/slideshow/demo4.jpg", alt: "スライド写真 4" }
   ]
 };
