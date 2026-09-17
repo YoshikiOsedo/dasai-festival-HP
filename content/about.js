46ddcb706d9387a63fc60f0f17a03f23d5c2ba18
大脊戸 慶揮 <qinghuidajihu07311031@gmail.com>
2026-09-18 01:58:11 +0900

    デーサイ企画部の紹介欄を追加

---
+++ content/about.js
@@ -0,0 +1,7 @@
+// 掲載情報.mdの「デーサイ企画部とはどんな集団？」を反映します。
+window.festivalContent = window.festivalContent || {};
+window.festivalContent.about = {
+  "heading": "デーサイ企画部とはどんな集団？",
+  "body": "Comming Soon ..."
+};
+
